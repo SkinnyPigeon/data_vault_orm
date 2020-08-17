2ea46d6b42685f4672f5ec100f732f25d7c76c67
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2020-08-17 13:30:33 +0100

    working on the new system for splitting data into dv

---
+++ api.py
@@ -0,0 +1,67 @@
+from sqlalchemy import create_engine, MetaData, insert
+from sqlalchemy.orm import sessionmaker, load_only
+from db import Base, Patient, Hub_Person, Hub_Location, Person_Location_Link, Sat_Location_Patient_Address, Sat_Person_Patient_Details
+import pandas as pd
+
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+project_folder = os.path.expanduser('~/code/dv_testing/')
+load_dotenv(os.path.join(project_folder, '.env'))
+PASSWORD = os.getenv('PASSWORD')
+
+engine = create_engine('postgresql://postgres:{}@localhost:5434/testing_v6'.format(PASSWORD), echo='debug')
+
+con = engine.connect()
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+query = session.query(Patient).filter(Patient.serums_id == 1)
+print(query.all())
+
+df = pd.read_sql(query.statement, con=con)
+print(df)
+
+patient_csv = pd.read_csv('./patient.csv')
+print(patient_csv)
+
+operations_csv = pd.read_csv('./operations.csv')
+print(operations_csv)
+
+for row, value in operations_csv.iterrows():
+  table = value['table']
+  column = value['column']
+  destination = value['destination']
+  hub = value['hub']
+  keys = value['keys']
+  keys = keys.split('::')
+  links = value['links']
+  links = links.split('::')
+
+  print(table)
+  print(column)
+  print(destination)
+  print(hub)
+  print(keys)
+  print(links)  
+
+  
+
+
+### Helper functions
+
+def get_hub(destination_table):
+    if "sat_person_" in destination_table:
+        return "hub_person"
+    elif "sat_location_" in destination_table:
+        return "hub_location"
+
+def get_class_by_tablename(table_fullname):
+  for class_name in Base._decl_class_registry.values():
+    if hasattr(class_name, '__table__') and class_name.__table__.fullname == table_fullname:
+      return class_name
+
+def insert_record(class_name):
+    print(class_name)
--- db.py
+++ db.py
@@ -26,9 +26,25 @@ class Hub_Location(Base):
     id = Column(Integer, primary_key=True)
     serums_id = Column(Integer)
 
+class Hub_Object(Base):
+    __tablename__ = 'hub_object'
+    id = Column(Integer, primary_key=True)
+    serums_id = Column(Integer)
+    operations_id = Column(Integer)
+
 class Person_Location_Link(Base):
     __tablename__ = 'person_location_link'
     person_id = Column(Integer, ForeignKey(Hub_Person.id), primary_key=True)
+    location_id = Column(Integer, ForeignKey(Hub_Location.id), primary_key=True)
+
+class Person_Object_Link(Base):
+    __tablename__ = 'person_object_link'
+    person_id = Column(Integer, ForeignKey(Hub_Person.id), primary_key=True)
+    object_id = Column(Integer, ForeignKey(Hub_Object.id), primary_key=True)
+
+class Object_Location_Link(Base):
+    __tablename__ = 'object_location_link'
+    object_id = Column(Integer, ForeignKey(Hub_Object.id), primary_key=True)
     location_id = Column(Integer, ForeignKey(Hub_Location.id), primary_key=True)
 
 class Sat_Person_Patient_Details(Base):
@@ -45,12 +61,31 @@ class Sat_Location_Patient_Address(Base):
     postcode = Column(String)
     hub_id = Column(Integer, ForeignKey(Hub_Location.id))
 
+class Sat_Object_Patient_Operations(Base):
+    __tablename__ = 'sat_object_patient_operations'
+    id = Column(Integer, primary_key=True)
+    operation_id = Column(Integer)
+    description = Column(String)
+
 class Patient(Base):
     __tablename__ = 'patient'
-    serums_id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
+    serums_id = Column(Integer)
     name = Column(String)
-    age = Column(Integer, primary_key=True)
+    age = Column(Integer)
     address = Column(String)
     postcode = Column(String)
+
+class Operations(Base):
+    __tablename__ = 'operations'
+    id = Column(Integer, primary_key=True)
+    serums_id = Column(Integer)
+    operation_id = Column(Integer)
+    description = Column(String)
+
+class Patient_Operations(Base):
+    __tablename__ = 'patient_operations'
+    serums_id = Column(Integer, ForeignKey(Patient.id), primary_key=True)
+    operations_id = Column(Integer, ForeignKey(Operations.id), primary_key=True)
 
 Base.metadata.create_all(engine)
